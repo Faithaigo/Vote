dc37fb52de29768a81d2a4ab954e26304867c6a2
ma-za-kpe <makpalyy@gmail.com>
2018-07-12 17:31:11 +0300

    add jquery

--- js/ballot.js
+++ js/ballot.js
@@ -1,25 +1,34 @@
-//ui logic
+
+//business Logic
+
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCi4-EJM7J-uCX7-VEd7dw1h7VCX738pQ4",
-    authDomain: "vote-5d11b.firebaseapp.com",
-    databaseURL: "https://vote-5d11b.firebaseio.com",
-    projectId: "vote-5d11b",
-    storageBucket: "vote-5d11b.appspot.com",
-    messagingSenderId: "917323240630"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyCi4-EJM7J-uCX7-VEd7dw1h7VCX738pQ4",
+  authDomain: "vote-5d11b.firebaseapp.com",
+  databaseURL: "https://vote-5d11b.firebaseio.com",
+  projectId: "vote-5d11b",
+  storageBucket: "vote-5d11b.appspot.com",
+  messagingSenderId: "917323240630"
+};
+firebase.initializeApp(config);
 
 
-var name = $("#head-prefer");
+//UI Logic
 
-const dbRefObject = firebase.database().ref().child("#head-prefer");
+$(document).ready(function() {
+  var count = 0;
 
-//dbRefObject.on('value', snap => console.log(snap.val()));
-function writeUserData(userId, name, email, imageUrl) {
-  firebase.database().ref('users/' + userId).set({
-    username: name,
-    email: email,
-    profile_picture : imageUrl
+  var countVotes = $("#count").val();
+
+  var click = $("#hp").click(function() {
+    //alert("Hello.");
+    count++;
+    $("#counter").html("My current count is: "+count);
   });
-}
+
+  var voteCountRef = firebase.database().ref('node/votes');
+  voteCountRef.on('value', function(snapshot) {
+    //updateStarCount(countVotes, snapshot.val());
+    return (currentClicks || 0) + 1;
+  });
+})
